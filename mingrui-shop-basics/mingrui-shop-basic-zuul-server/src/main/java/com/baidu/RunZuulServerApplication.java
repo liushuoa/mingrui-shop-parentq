3428d637315ffc56f9b6b75a79af411a101d13ba
liushuoa <liushuo.1>
2021-01-19 22:40:23 +0800

    新写的项目

---
+++ mingrui-shop-basics/mingrui-shop-basic-zuul-server/src/main/java/com/baidu/RunZuulServerApplication.java
@@ -0,0 +1,17 @@
+package com.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class RunZuulServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunZuulServerApplication.class);
+    }
+}
---
+++ mingrui-shop-commoms/mingrui-shop-common-core/src/main/java/com/baidu/shop/status/HTTPStatus.java
@@ -0,0 +1,8 @@
+package com.baidu.shop.status;
+
+public class HTTPStatus {
+    public static final int OK = 200;//成功
+    public static final int ERROR = 500;//失败
+    public static final int PARAMS_VALIDATE_ERROR = 5002;
+}
+
