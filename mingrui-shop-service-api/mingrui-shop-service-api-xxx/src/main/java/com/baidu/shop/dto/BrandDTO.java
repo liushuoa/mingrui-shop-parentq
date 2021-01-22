ecee04cf8f13d563955cf6888c365c8250c640c3
liushuoa <liushuo.1>
2021-01-22 19:37:36 +0800

    品牌查询分页

---
+++ mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/dto/BrandDTO.java
@@ -0,0 +1,30 @@
+package com.baidu.shop.dto;
+
+import com.baidu.shop.base.BaseDTO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@ApiModel(value = "品牌DTO")
+@Data
+public class BrandDTO extends BaseDTO {
+
+    @ApiModelProperty(value = "品牌主键",example = "1")
+    @NotNull(message = "主键不能为空")
+    private Integer id;
+
+    @ApiModelProperty(value = "品牌名称")
+    @NotNull(message = "品牌名称不能为空")
+    private String name;
+
+    @ApiModelProperty(value = "品牌 logo")
+    private String image;
+
+    @ApiModelProperty(value = "品牌首字母")
+    private Character letter;
+
+    @ApiModelProperty(value = "品牌id集合")
+    private String categories;
+}
